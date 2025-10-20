d41dbaac5b5a30d2a1e9f4206716307c1405be2a
Hydra88976 <136618530+Hydra88976@users.noreply.github.com>
2025-10-20 22:38:22 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options)
+driver.get("https://www.google.com")
+print(driver.title)
+driver.quit()
